6e154e6b0aa94b81d6231036eaccb5e7edbe6925
DavideAtzo <atzo9477@gmail.com>
2023-02-24 16:09:39 +0100

    prova gioco

--- es-dadi/js/main.js
+++ es-dadi/js/main.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const btnGioca = document.getElementById('gioca');
+let puntGiocatore = document.getElementById('punt-giocatore');
+let puntComputer = document.getElementById('punt-pc');
+const risultato = document.getElementById('risultato');
+
+btnGioca.addEventListener('click',
+    function(){
+        puntGiocatore = Math.floor(Math.random() * 6);
+        puntComputer = Math.floor(Math.random() * 6);
+
+        if (puntGiocatore > puntComputer){
+            console.log('hai vinto!');
+          } 
+          else if (puntGiocatore < puntComputer) {
+            console.log('ahahah hai perso!');
+          } 
+          else{
+            console.log('gioca di nuovo');
+          }
+            
+    }
+)
