74fbffddd1515ba880f37a6c364c0d698405ac99
DavideAtzo <atzo9477@gmail.com>
2023-02-24 16:17:18 +0100

    aggiunta commenti es. dadi

--- es-dadi/js/main.js
+++ es-dadi/js/main.js
@@ -1,27 +1,29 @@
 'use strict';
-
+//Dichiarazione variabili
 const btnGioca = document.getElementById('gioca');
 let puntGiocatore = document.getElementById('punt-giocatore');
 let puntComputer = document.getElementById('punt-pc');
 const risultato = document.getElementById('risultato');
-
+// evento del calcolo random sul click del bottone 
 btnGioca.addEventListener('click',
     function(){
         puntGiocatore = Math.floor(Math.random() * 6 + 1);
         puntComputer = Math.floor(Math.random() * 6 + 1);
-
+        // se vince il giocatore 
         if (puntGiocatore > puntComputer){
             document.getElementById('punt-giocatore').innerHTML = puntGiocatore;
             document.getElementById('punt-pc').innerHTML = puntComputer;
             risultato.innerText = ('hai vinto');
             console.log('hai vinto!');
           } 
+        //   se vince il computer 
           else if (puntGiocatore < puntComputer) {
             document.getElementById('punt-giocatore').innerHTML = puntGiocatore;
             document.getElementById('punt-pc').innerHTML = puntComputer;
             risultato.innerText = ('ahahah hai perso!');
             console.log('ahahah hai perso!');
           } 
+        //   se pareggiano 
           else{
             document.getElementById('punt-giocatore').innerHTML = puntGiocatore;
             document.getElementById('punt-pc').innerHTML = puntComputer;
