20fc003747670218a7423e3817991c273360253d
DavideAtzo <atzo9477@gmail.com>
2023-02-24 16:18:24 +0100

    aggiunta commenti es mail + final commit

--- es-mail/js/main.js
+++ es-mail/js/main.js
@@ -1,10 +1,10 @@
 'use strict';
-
+//Dichiarazione variabili
 const yourMail = prompt('Inserirsci la tua mail');
 const listMail = ['fragola@gmail.com', 'banana@gmail.com', 'pesca@gmail.com', 'kiwi@gmail.com', 'limone@gmail.com'];
 
 console.log(listMail);
-
+// ciclo che controllerà se la mail è presente tra i privilegiati 
 for (let i = 0; i < listMail.length; i++) {
     if (yourMail === listMail[i]) {
         console.log ('complimenti, sei tra i privilegiati');
