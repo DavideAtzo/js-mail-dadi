c7c7f09dd38ec9bb909673fdecb538978b375999
DavideAtzo <atzo9477@gmail.com>
2023-02-24 15:13:36 +0100

    eserciz. mail final commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-alert ('ciao')
+const yourMail = prompt('Inserirsci la tua mail');
+const listMail = ['fragola@gmail.com', 'banana@gmail.com', 'pesca@gmail.com', 'kiwi@gmail.com', 'limone@gmail.com'];
+
+console.log(listMail);
+
+for (let i = 0; i < listMail.length; i++) {
+    if (yourMail === listMail[i]) {
+        console.log ('complimenti, sei tra i privilegiati');
+    }
+    else(yourMail != listMail[i])
+        console.log('Tu non puoi passare!!');
+        break;
+}
+
+
